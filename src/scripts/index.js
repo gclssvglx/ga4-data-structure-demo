7ab7d30b05ac5643aff8619d7d997a7eb44967ee
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2022-07-08 09:20:10 +0100

    Install GA4

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,12 @@
 window.onload = function (event) {
   console.log("Site loaded...")
+
+  window.dataLayer = window.dataLayer || []
+
+  function gtag() {
+    window.dataLayer.push(arguments)
+  }
+
+  gtag('js', new Date())
+  gtag('config', 'GTM-MGR2L95')
 }
